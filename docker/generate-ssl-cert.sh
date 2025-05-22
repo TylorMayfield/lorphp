8f4368de7e15044a6b2baecf2eb06c122f8bf99c
Tylor Mayfield <tylor@tsohg.com>
2025-05-22 18:18:30 -0400

    byebye docker

--- docker/generate-ssl-cert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Generate self-signed SSL certificate for development
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout docker/ssl/key.pem \
-    -out docker/ssl/cert.pem \
-    -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=localhost"
